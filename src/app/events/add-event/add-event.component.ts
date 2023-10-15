94b1f16ddda8547654debe724f50d9a512be95b8
Darren Thompson <darren.thompson200@gmail.com>
2023-10-15 15:20:03 -0400

    Modify add-event

--- src/app/events/add-event/add-event.component.ts
+++ src/app/events/add-event/add-event.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { format, utcToZonedTime } from 'date-fns-tz';
+
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-event',
   templateUrl: './add-event.component.html',
   styleUrls: ['./add-event.component.scss'],
+  imports: [CommonModule, IonicModule],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddEventComponent  implements OnInit {
 
-  constructor() { }
+  constructor() {
+    const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const date = new Date();
+    const zonedTime = utcToZonedTime(date, userTimeZone);
+    console.log(format(zonedTime, 'MMM dd, yyyy HH:mm', {timeZone: userTimeZone}));
+  }
 
   ngOnInit() {}
 
